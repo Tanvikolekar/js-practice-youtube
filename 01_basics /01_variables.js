025e4cec201fc76f89b03f33ba078a2a49aa0997
Tanvi kolekar <166321885+Tanvikolekar@users.noreply.github.com>
2025-01-30 11:56:46 +0000

    variablesDone

---
+++ 01_basics /01_variables.js
@@ -0,0 +1,16 @@
+const amountId = 12345;
+let emailId = "tanvi@123";
+var password ="222123";
+accountCity ="Jaipur"
+
+let goodcity;  //output of undeclared variable is undefined 
+
+//amountId = 22052005; We can declare a const variable only once. so this is not allowed
+/* don't used var due to issues in the block scop and functional scope*/
+
+emailId ="tanu@22";
+password ="45555"
+accountCity ="pune"
+
+console.log(amountId);
+console.table([amountId,emailId,password,accountCity,goodcity]);
