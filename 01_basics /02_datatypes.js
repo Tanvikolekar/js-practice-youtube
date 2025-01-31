a5aba31a73b76593b012d5ce0b5f34a0ed3e3e83
Tanvi kolekar <166321885+Tanvikolekar@users.noreply.github.com>
2025-01-31 08:23:59 +0000

    datatypes

---
+++ 01_basics /02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as a new version
+
+//alert(3+3) it will not work because we are using node here not a browser 
+
+//number => range should be be 2 to the power 53
+//bigint => used for the large number scale eg:stock marketing,bank,etc
+//string=>" "
+//boolean =>true/false
+//undefined =>value is not defined
+//null=>empty standalone 
+//symbol=>to find uniqueness
+
+//object
+console.log(typeof undefined);  //o/p: undefined 
+console.log(typeof null); //o/p:object
+
+
+
