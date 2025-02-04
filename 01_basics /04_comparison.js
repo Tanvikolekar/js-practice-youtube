586bec80bb79b9c5238a7882b30155d825f3ce73
Tanvi kolekar <166321885+Tanvikolekar@users.noreply.github.com>
2025-02-04 13:30:16 +0000

    comparison

---
+++ 01_basics /04_comparison.js
@@ -0,0 +1,22 @@
+/*console.log(2==2);
+console.log(2!=2);
+console.log(2<=2);
+console.log(2>=2);
+console.log(2>2);
+console.log(2<2);*/
+
+// console.log("2" == 2);
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+// console.log(null < 1);
+// console.log(null > 1);
+// console.log(null == 1);
+
+console.log(undefined < 1);
+console.log(undefined > 1);
+console.log(undefined == 1);
+
+//=== :- this sign checks the value with its datatype 
+console.log("2" === 2);
+
