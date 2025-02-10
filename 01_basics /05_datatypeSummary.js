4bedf174fee968b8faa086de4f9940eb32900288
Tanvi kolekar <166321885+Tanvikolekar@users.noreply.github.com>
2025-02-10 09:28:26 +0000

    summaryOfDatatypes

---
+++ 01_basics /05_datatypeSummary.js
@@ -0,0 +1,29 @@
+/*Datatypes:- 
+1)Primitive : String,Number,Boolean,Null,Undefined 
+example:-*/
+const myId = 1234
+const myName = "tanvi"
+const myAnswer = true
+const myAnotherId = null
+
+let myEmailId ;
+
+/*2)Non-Primitive : object , function, array
+examples:*/
+let cartons = ["chotabheem","oggy","doremon","hatodi"]
+
+let myObj ={
+    name:"tanvi",
+    age:19
+}
+
+let myFunction = function(){
+    console.log("this is my funtion")
+}
+myFunction();
+
+//*****************************MEMORY***************************//
+/* stack memory for primmitive datatype
+   Heap memory for Non-primitive datatype*/
+
+
